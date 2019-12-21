d7a3005e5ea4e27e930c7d2bd7eabf817de488fe
nikhil-kuyya <nikhil.k@cconfigure.com>
2019-12-21 19:28:45 +0530

    first commit for setup for model with swagger

---
+++ my-proman-api/target/generated-sources/io/swagger/api/SignupApiController.java
@@ -0,0 +1,32 @@
+package io.swagger.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-21T19:22:03.194+05:30")
+
+@Controller
+public class SignupApiController implements SignupApi {
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public SignupApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    @Override
+    public Optional<ObjectMapper> getObjectMapper() {
+        return Optional.ofNullable(objectMapper);
+    }
+
+    @Override
+    public Optional<HttpServletRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
