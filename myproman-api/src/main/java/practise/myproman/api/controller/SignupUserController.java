d49755684b406912eb732a315a6a30ccff37f0a6
nikhil-kuyya <nikhil.k@cconfigure.com>
2019-12-22 17:31:38 +0530

    update the code setup for api swagger

--- myproman-api/src/main/java/practise/myproman/api/controller/SignupUserController.java
+++ myproman-api/src/main/java/practise/myproman/api/controller/SignupUserController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//import practise.myproman.api.model;
 
 @RestController
 @RequestMapping("/")
