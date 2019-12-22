4b639ecde589c293cbef5d155f115e0952af02de
nikhil-kuyya <nikhil.k@cconfigure.com>
2019-12-22 18:51:06 +0530

    singup controller setup

--- myproman-api/src/main/java/practise/myproman/api/controller/SignupUserController.java
+++ myproman-api/src/main/java/practise/myproman/api/controller/SignupUserController.java
@@ -1,18 +1,21 @@
 package practise.myproman.api.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-//import practise.myproman.api.model;
+import practise.myproman.api.model.SignupUserRequest;
+import practise.myproman.api.model.SignupUserResponse;
 
 @RestController
 @RequestMapping("/")
 public class SignupUserController {
 
-    // public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest
-    // SignupUserRequest) {
-    // return new SignupUserResponse(HttpStatus.OK);
-    // }
+    @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest SignupUserRequest) {
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
